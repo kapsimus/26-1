78d9ce3bb427c2b91a17f3bbae78146b2060876d
инженеры АВЗ <avz@GT.local>
2022-07-10 15:28:55 +0300

    final

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ class Track {
     int duration;
 
 public:
-    void setName(const std::string& name) {
+    void setName(const std::string& trackName) {
         if (!name.empty()) {
-            this->name = name;
+            name = trackName;
         }
     }
 
     void setDate(int year, int month, int day) {
         std::time_t t = time(nullptr);
         std::tm* tm = localtime(&t);
-        if (year <= tm->tm_year) {
+        if (year <= (tm->tm_year + 1900)) {
             date.tm_year = year - 1900;
         }
         if (month > 0 && month < 13) {
@@ -60,12 +60,17 @@ class Player {
         pause_mode,
         stop_mode
     };
-    mode playerMode = stop_mode;
-    int trackCount = 0;
-    int currentTrack = 0;
+    mode playerMode;
+    int trackCount;
+    int currentTrack;
 
 public:
 
+    Player() {
+       trackCount = 0;
+       currentTrack = -1;
+       playerMode = stop_mode;
+    }
     void addTrack(Track track) {
         trackList.push_back(track);
         trackCount = trackList.size();
@@ -76,39 +81,105 @@ public:
             if (playerMode == stop_mode) {
                 std::string name;
                 std::cout << "Enter track name: ";
-                std::cin >> name;
+                std::getline(std::cin, name);
                 for (int i = 0; i < trackCount; i++) {
                     if (trackList[i].getName() == name) {
                         currentTrack = i;
                         std::tm* tm = trackList[i].getDate();
                         std::cout << "Played track:" << std::endl;
-                        std::cout << trackList[i].getName() << std::put_time(tm, "%Y:%M:%D") << " duration: " <<
+                        std::cout << trackList[i].getName() << " " << std::put_time(tm, "%Y:%m:%d") << " duration: " <<
                                   trackList[i].getDuration() / 60 << " min, " << trackList[i].getDuration() % 60 << " sec." << std::endl;
                         break;
                     }
                 }
+                playerMode = play_mode;
             }
             if (playerMode == pause_mode) {
-
+                std::tm* tm = trackList[currentTrack].getDate();
+                std::cout << "Played track:" << std::endl;
+                std::cout << trackList[currentTrack].getName() << " " << std::put_time(tm, "%Y:%m:%d") <<
+                    " duration: " << trackList[currentTrack].getDuration() / 60 << " min, " <<
+                    trackList[currentTrack].getDuration() % 60 << " sec." << std::endl;
+                playerMode = play_mode;
             }
         }
 
     }
 
     void pause() {
-
+        if (playerMode == play_mode) {
+            std::cout << "Track paused." << std::endl;
+            playerMode = pause_mode;
+        }
     }
 
     void stop() {
+       if (playerMode == play_mode) {
+           std::cout << "Track stopped." << std::endl;
+           playerMode = stop_mode;
+       }
+    }
 
+    void next() {
+        if (trackCount > 1) {
+            int selectTrack;
+            do {
+                selectTrack = std::rand() % trackCount;
+            } while (selectTrack == currentTrack);
+            currentTrack = selectTrack;
+            playerMode = pause_mode;
+            play();
+        } else if (trackCount == 1) {
+            currentTrack = 0;
+            playerMode = pause_mode;
+            play();
+        }
     }
 
     void exit() {
-
+        return;
     }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string op;
+    Track tempTrack;
+    Player* player = new Player();
+    //create tracklist
+    tempTrack.setName("Song 1");
+    tempTrack.setDate(1999, 01, 01);
+    tempTrack.setDuration(300);
+    player->addTrack(tempTrack);
+    tempTrack.setName("Song 2");
+    tempTrack.setDate(2001, 10, 02);
+    tempTrack.setDuration(200);
+    player->addTrack(tempTrack);
+    tempTrack.setName("Song 3");
+    tempTrack.setDate(2002, 03, 11);
+    tempTrack.setDuration(310);
+    player->addTrack(tempTrack);
+    tempTrack.setName("Song 4");
+    tempTrack.setDate(2020, 11, 21);
+    tempTrack.setDuration(110);
+    player->addTrack(tempTrack);
+
+    do {
+        std::cout << "Enter command: ";
+        std::getline(std::cin, op);
+        if (op == "play") {
+            player->play();
+        }
+        if (op == "pause") {
+            player->pause();
+        }
+        if (op == "stop") {
+            player->stop();
+        }
+        if (op == "next") {
+            player->next();
+        }
+
+    } while (op != "exit");
+    delete player;
 }
